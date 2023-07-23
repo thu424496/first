eaaf2dfd6d67a1fe4c98ea05581e2a565a86ba69
thu424496 <124645406+thu424496@users.noreply.github.com>
2023-07-23 09:47:02 +0800

    add select sort

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-print("hello world")
-username = input("please input your username:")
-password = input('please input your password:')
-# if username and password are invalid next
+## select sort
+def selectionSort(arr):
+    for i in range(len(arr) - 1):
+        # 记录最小数的索引
+        minIndex = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+        # i 不是最小数时，将 i 和最小数进行交换
+        if i != minIndex:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    return arr
 
 
 ## sort
